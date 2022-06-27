e564833570e681cb25dd75178dc7a7cfe4f24c31
liuaiying <1425577366@qq.com>
2022-06-27 20:45:43 +0800

    加载器 - 处理less文件

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,8 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 // css    yarn add style-loader css-loader -D
 // 重复
+
+//less    yarn add less less-loader -D
 module.exports = {
   mode: 'development',
   entry: './src/main.js', // 入口
@@ -55,6 +57,15 @@ module.exports = {
       },
 
       // 处理less，loader的规则
+      {
+        test: /\.less$/, // 匹配执行类型的文件
+        // 使用less-loader, 让webpack处理less文件, 内置还会用less翻译less代码成css内容
+        // 执行的顺序 less-loader css-loader style-loader
+        // less-loader 先把less代码转换成css
+        // css-loader 再把css代码转换成webpack 可以识别的js代码
+        // style-loader 在把css代码插入到 dom中
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
     ],
   },
 };
